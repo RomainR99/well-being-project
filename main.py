a0d64af085df400a54020007c9a58a1c3285f1ae
Hakim <horairyhakim@gmail.com>
2026-05-18 16:54:26 +0200

    feature: now we can standard scale our X input

--- main.py
+++ main.py
@@ -1,14 +1,19 @@
 import pandas
-
+from sklearn.preprocessing import StandardScaler
 
 def load_data(csv_path, target_col="target"):
-		df = pandas.read_csv(csv_path)
-		X = df.drop(columns=[target_col])
-		Y = df[target_col]
-		return X, Y 
+	df = pandas.read_csv(csv_path)
+	X = df.drop(columns=[target_col])
+	Y = df[target_col]
+	return X, Y 
+
+def nomalize(X):
+	standard_scaler_object = StandardScaler()
+	X_normalized = standard_scaler_object.fit_transform(X)
+	return standard_scaler_object, X_normalized
 
 
 if __name__ == "__main__":
 	X, Y = load_data(csv_path="bienetre.csv")
-	print(X.head())
-	print(Y)
+	standard_scaler_object, X_normalized = nomalize(X)
+	print(X_normalized)
