d759f8117d285624b8930fa04beeb9ef60ffa17e
Hakim <horairyhakim@gmail.com>
2026-05-19 12:08:54 +0200

    feature : now we have a artisanal implementation of grid search cv algorihtm on a knn

--- main.py
+++ main.py
@@ -16,16 +16,37 @@ def nomalize(X):
 	return standard_scaler_object, X_normalized
 
 
-def evaluate(X_normalized, Y, n_neighbors=5, n_splits=10):
+def evaluate(X_normalized, Y, n_neighbors=5, n_splits=10, scoring="f1_weighted"):
 	cross_validation_object = StratifiedKFold(n_splits=n_splits, shuffle=True, random_state=42)
 	knn_object = KNeighborsClassifier(n_neighbors=n_neighbors)
 
-	scores = cross_val_score(knn_object, X_normalized, Y, cv=cross_validation_object, scoring="f1_weighted")
-	print(f"F1-score moyen {scores.mean()} +/- {scores.std()}")
+	scores = cross_val_score(knn_object, X_normalized, Y, cv=cross_validation_object, scoring=scoring)
+	print(f"N neighbors : {n_neighbors}")
+	print(f"{scoring} moyen {scores.mean()} +/- {scores.std()}")
+	print("-"*20)
 	return scores
+
+
+def find_optimal_kvalue(X_normalized, Y, n_splits=10, scoring="f1_weighted", n_neighbors_range=range(1, 100,2)):
+	"""Grid Search CV"""
+
+	dict_of_k_scores = {}
+	
+	for n_neighbors in n_neighbors_range:
+		scores = evaluate(X_normalized, Y, n_neighbors=n_neighbors, n_splits=n_splits, scoring=scoring)
+		current_mean_score = scores.mean()
+
+		dict_of_k_scores[n_neighbors] = current_mean_score
+
+	optimal_n_neighbors = max(dict_of_k_scores, key=dict_of_k_scores.get)
+	print("-"*100)
+	print(f"The optimal N neighbors value is {optimal_n_neighbors}")
+	print("-"*100)
+	return optimal_n_neighbors
 
 
 if __name__ == "__main__":
 	X, Y = load_data(csv_path="bienetre.csv")
 	standard_scaler_object, X_normalized = nomalize(X)
-	scores = evaluate(X_normalized, Y, n_neighbors=5, n_splits=10)
+
+	optimal_n_neighbors = find_optimal_kvalue(X_normalized, Y, scoring="f1_weighted")
